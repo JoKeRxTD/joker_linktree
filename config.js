3ff9c0316fbc5b4db324814de57aa29df3f48d9d
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2023-09-12 21:28:05 +0100

    config file for eat button, log, description

---
+++ config.js
@@ -0,0 +1,22 @@
+// Logo
+Logo= "https://i.imgur.com/0Q4zv9B.png"
+
+// Description
+Description= "My First Config Description"
+
+// Button 1: 1
+Title= "My First Config"
+Link= "https://www.google.com"
+
+// Button 2: 2
+Title2= "My Second Config"
+Link2= "https://www.google.com"
+
+// Button 3: 3
+Title3= "My Third Config"
+Link3= "https://www.google.com"
+
+// Button 4: 4
+Title4= "My Fourth Config"
+Link4= "https://www.google.com"
+
